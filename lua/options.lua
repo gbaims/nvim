f27ebf878e99b19ceb72aafab961bb5c8bd5f81e
Gabriel Baims <gabriel.a.baims@gmail.com>
2022-06-28 02:16:00 -0300

    set history, cursor and rulers options

--- lua/options.lua
+++ lua/options.lua
@@ -6,8 +6,6 @@ vim.cmd 'syntax on'
 
 local o = vim.opt
 
-o.number = true
-o.relativenumber = true
 
 -- Tabs and whitespaces
 o.tabstop = 2                   -- Specifies the width of tab character
@@ -19,3 +17,14 @@ o.autoindent = true             -- New lines inherit indentation of previous lin
 o.list = true                   -- Show invisible chars
 o.listchars = 'tab:»·,trail:·'  -- Set listchars for tabs and trailing spaces
 o.wrap = false                  -- Don't wrap lines
+
+-- History, cursor and rulers
+o.history = 50                  -- Just remember last 50 commands
+o.laststatus = 2                -- Always display the status line
+o.relativenumber = true         -- Show relative numbers ...
+o.number = true                 -- ... but show current line number
+o.ruler = true                  -- Show the cursor position all the time
+o.cursorline = true             -- Highlight current cursor line
+o.scrolloff = 8                 -- Start scrolling before reaches the end
+o.signcolumn = 'auto'           -- Extra column for linting, lsp or git signs if necessary
+o.colorcolumn = '100'           -- Avoid exceeding that much columns
