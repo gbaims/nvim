2c38213441a6fac5540e1aa368f34bd33d98a808
Gabriel Baims <gabriel.a.baims@gmail.com>
2022-06-28 18:36:00 -0300

    linting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local empty_path = fn.empty(fn.glob(install_path)) > 0
 if empty_path then
-  just_installed = fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+  JUST_INSTALLED = fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
   vim.cmd 'packadd packer.nvim'
 end
 
 -- Initialize and configure packer
 local packer = require 'packer'
-packer.init { display = { open_fn = function() return require('packer.util').float { border = 'rounded' } end }}
+packer.init { display = { open_fn = function() return require('packer.util').float { border = 'rounded' } end } }
 packer.startup(function(use)
   -- Use plugin and require configuration
-  function reuse(name)
+  local function reuse(name)
     local file = string.gsub(name, '%p', '-')
     local path = string.format('plugins/%s', file)
     local _, module = pcall(require, path)
@@ -46,7 +46,7 @@ packer.startup(function(use)
   reuse 'hrsh7th/cmp-nvim-lsp'
   reuse 'neovim/nvim-lspconfig'
 
-  if just_installed then
+  if JUST_INSTALLED then
     packer.sync()
   end
 end)
